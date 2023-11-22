823274bdcda441cb6e571c42097139a54e3fe09a
othmane <otman_go@hotmail.com>
2023-11-21 20:10:22 -0500

    mise a jour de app.py

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ def home():
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data=request.json['data']
+    data=request.get_json(force=True)
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=model.predict(new_data)
+    features = np.array(data['features']).reshape(1, -1)
+    #data=int(np.array(list(data.values())).reshape(1,-1))
+    output=model.predict(features)
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=model.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+#@app.route('/predict',methods=['POST'])
+#def predict():
+    #data=[float(x) for x in request.form.values()]
+    #final_input=model.transform(np.array(data).reshape(1,-1))
+    #print(final_input)
+    #output=Classmodel.predict(final_input)[0]
+    #return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
 
